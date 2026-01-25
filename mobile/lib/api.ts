9712c33e2d810850166fd2a5190eb3e82e52a726
Cysteine12 <gbengapeace2001@gmail.com>
2026-01-25 23:11:04 +0100

    feat(auth): apply review changes

--- mobile/lib/api.ts
+++ mobile/lib/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: { 'Content-Type': 'application/json' },
+  timeout: 30000,
 });
 
 export default API;
