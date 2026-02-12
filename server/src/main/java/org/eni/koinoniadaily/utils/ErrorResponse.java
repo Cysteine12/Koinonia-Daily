8b8167d2f5cd7d20937399c354edf13abdd65f88
Cysteine12 <gbengapeace2001@gmail.com>
2026-02-12 13:35:35 -0800

    fix: update error response format

--- server/src/main/java/org/eni/koinoniadaily/utils/ErrorResponse.java
+++ server/src/main/java/org/eni/koinoniadaily/utils/ErrorResponse.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
-@Getter
+//@Getter
 @SuperBuilder
 public class ErrorResponse extends ApiResponse {
   
@@ -14,6 +14,8 @@ public class ErrorResponse extends ApiResponse {
   private final String error;
   
   private final String path;
-  
+
+  private final String errorCode;
+
   private final Instant timestamp;
 }
