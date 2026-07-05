a3ea426611023ff364d9a468ec87f88ee141b043
Cysteine12 <gbengapeace2001@gmail.com>
2026-07-05 13:06:16 +0100

    fix(sentry): correct config & protect sensitive data

--- mobile/lib/logger.ts
+++ mobile/lib/logger.ts
@@ -2,8 +2,9 @@ import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
     dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
-    sendDefaultPii: true,
-    tracesSampleRate: 1.0
+    sendDefaultPii: false,
+    tracesSampleRate: __DEV__ ? 1.0 : 0.2,
+    environment: __DEV__ ? 'development' : 'production'
 })
 
 export default Sentry;
